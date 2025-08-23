ddee4de94f5bc28542c5c88390286e2b025a91d8
Chahat <fst.phoenix987@gmail.com>
2025-08-23 19:05:55 +0530

    Time: 2 ms (24.19%), Space: 42.6 MB (32.83%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int myAtoi(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sb.append(s.charAt(i));
+            i++;
+        }
+
+        while (i < n) {
+            char ch = s.charAt(i);
+            if (ch >= '0' && ch <= '9') {
+                sb.append(ch);
+            } else {
+                break; 
+            }
+            i++;
+        }
+        if (sb.length() == 0 || sb.toString().equals("+") || sb.toString().equals("-")) {
+            return 0;
+        }
+
+        try {
+            return Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return sb.charAt(0) == '-' ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+    }
+}
