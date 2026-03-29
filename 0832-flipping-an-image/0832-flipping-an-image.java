81829443a1db8e9fe067f1227d83f0eee1726eae
Chahat <fst.phoenix987@gmail.com>
2026-03-29 23:30:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -4,14 +4,11 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
                 arr[i][j]=nums[i][arr[0].length-j-1];
-            }
-        }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
                 if(arr[i][j]==1) arr[i][j]=0;
                 else arr[i][j]=1;
             }
         }
+        
 
         return arr;
     }
