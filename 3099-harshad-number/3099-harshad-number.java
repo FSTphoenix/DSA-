f7eb4c55fc9833daab532e82274e0fbdc4c98e5f
Chahat <fst.phoenix987@gmail.com>
2026-05-02 17:34:16 +0530

    Time: 0 ms (100%), Space: 42 MB (61.76%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    static int f(int n){
+        int sum=0;
+        while(n!=0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        if(x%f(x)==0) return f(x);
+        return -1;
+    }
+}
